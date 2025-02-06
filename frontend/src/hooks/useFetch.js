8a174cec4177d10b3190649a0f57838b2e1fadfe
BY-ee <by-ee@naver.com>
2025-02-07 01:56:56 +0900

    ♻ refactor[useFetch]: useFetch 훅에 fetch 요청을 다시 보내는 함수를 추가

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 /**
  * 서버에 fetch 요청을 보내는 리액트 훅입니다.
@@ -6,10 +6,11 @@ import React, { useEffect, useState } from 'react';
  * @param {String} endpoint - API 호출의 엔드포인트입니다.
  * @param {'POST' | 'PUT' | 'PATCH' | 'DELETE'} method - method입니다. 기본값은 "GET"입니다.
  * @param {Object} request - 서버에 전송할 데이터 객체입니다. "GET" 요청에서는 사용되지 않습니다.
- * @returns {{ data: any, error: string | null, loading: boolean }}
+ * @returns {{ data: any, error: string | null, loading: boolean, refetch: Function }}
  * - `data`: 서버에서 반환된 데이터
  * - `error`: 발생한 오류 메시지(없으면 `null`)
  * - `loading`: 데이터 로딩 상태(`true` 또는 `false`)
+ * - `refetch`: 데이터 재요청 함수
  */
 function useFetch(endpoint, method = 'GET', request = {}) {
   const [data, setData] = useState(null);
@@ -18,17 +19,18 @@ function useFetch(endpoint, method = 'GET', request = {}) {
   const stringifiedRequest = JSON.stringify(request); // 전송 객체를 문자열 형식으로 변환
   const baseUrl = 'http://localhost:8090'; // 서버 url
 
-  useEffect(() => {
-    const controller = new AbortController(); // fetch 요청의 중단 신호를 제어하는 컨트롤러
-    setLoading(true);
+  // fetch 요청을 보내는 함수
+  const fetchData = useCallback(
+    async (signal) => {
+      setLoading(true);
+      setError(null);
 
-    const fetchData = async () => {
       try {
         const response = await fetch(`${baseUrl}${endpoint}`, {
           method,
           headers: { 'Content-Type': 'application/json' },
           body: method !== 'GET' ? stringifiedRequest : undefined,
-          signal: controller.signal, // fetch 요청 중단 신호를 받기 위한 signal 연결
+          signal, // fetch 요청 중단 신호를 받기 위한 signal 연결
         });
 
         if (!response.ok) {
@@ -38,23 +40,32 @@ function useFetch(endpoint, method = 'GET', request = {}) {
         const json = await response.json();
         setData(json);
       } catch (error) {
-        // AbortError 에러는 무시 (이전 fetch 요청을 중단하는 정상적인 흐름, 의도된 에러)
         if (error.name !== 'AbortError') {
           setError(error.message);
         }
       } finally {
         setLoading(false);
       }
-    };
+    }, // useCallback
+    [endpoint, method, stringifiedRequest]
+  ); // fetchData
 
-    fetchData();
+  useEffect(() => {
+    const controller = new AbortController(); // fetch 요청의 중단 신호를 제어하는 컨트롤러
+    fetchData(controller.signal);
 
     return () => {
-      controller.abort(); // fetch 요청이 진행 중인 경우 fetch를 중단시키고 AbortError를 발생시킴
+      controller.abort(); // 진행중인 fetch를 중단시키고 AbortError를 발생시킴
     };
-  }, [endpoint, method, stringifiedRequest]);
+  }, [fetchData]);
 
-  return { data, error, loading };
+  // fetch 요청을 다시 보내는 함수
+  const refetch = () => {
+    const controller = new AbortController();
+    fetchData(controller.signal);
+  };
+
+  return { data, error, loading, refetch };
 }
 
 export default useFetch;
