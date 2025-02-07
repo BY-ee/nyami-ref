2ca4bb9b59525ebd37ffd056d63b2f00b1c3a254
BY-ee <by-ee@naver.com>
2025-02-08 04:45:52 +0900

    ♻ refactor[Container]: 컨테이너 컴포넌트에 width, height를 적용할 수 있도록 수정

--- frontend/src/components/container/Container.jsx
+++ frontend/src/components/container/Container.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Container.module.css';
 
-const Container = ({ children }) => {
+const Container = ({ children, width, height }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}>{children}</div>
+      <div className={styles.wrapper} style={{ width, height }}>
+        {children}
+      </div>
     </div>
   );
 };
